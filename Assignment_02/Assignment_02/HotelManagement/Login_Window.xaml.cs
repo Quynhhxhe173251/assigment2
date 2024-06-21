8574ed1bb528fb510d439efe4420dd37fec58083
Quynhhxhe173251 <quynhhxhe173251@fpt.edu.vn>
2024-06-21 13:23:00 +0700

    assgiment2

---
+++ Assignment_02/Assignment_02/HotelManagement/Login_Window.xaml.cs
@@ -0,0 +1,63 @@
+﻿using HotelManagement.Models;
+using RepositoryPattern1;
+using System.Linq;
+using System.Windows;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+namespace MiniHotelManagement
+{
+    public partial class Login_Window : Window
+    {
+        private readonly CustomerRepository _customerRepository;
+
+     
+        public Login_Window(CustomerRepository customerRepository)
+        {
+
+            InitializeComponent();
+            _customerRepository = customerRepository;
+        }
+
+        private void txtUser_TextChanged(object sender, System.Windows.Controls.TextChangedEventArgs e)
+        {
+
+        }
+
+        public static IConfiguration Configuration { get; private set; }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+            var users = _customerRepository.GetAllCustomers();
+            var user = users.FirstOrDefault(u => u.EmailAddress == txtUser.Text && u.Password == txtPass.Password);
+
+            if (user != null)
+            {
+
+                    this.Hide();
+                    CurrentUser.LoggedInUser = user;
+                    UserProfile roomManager = (App.Current as App)?.GetProfileWindow();
+                    roomManager.Show();               
+            }
+            else
+            {
+                if (txtUser.Text == "admin@FUMiniHotelSystem.com" && txtPass.Password == "@@abc123@@")
+                {
+                    this.Hide();
+                    MainWindow mainWindow = (App.Current as App)?.GetMainWindow();
+                    mainWindow?.Show();
+                } else
+                {
+
+                   MessageBox.Show("User name or password is incorrect.");
+                }
+            }
+        }
+
+        
+    }
+}
---
+++ Assignment_02/Assignment_02/RepositoryPattern1/CustomerRepository.cs
@@ -0,0 +1,60 @@
+﻿using HotelManagement.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RepositoryPattern1
+{
+    public class CustomerRepository : ICustomerRepository
+    {
+        private readonly FuminiHotelManagementContext _context;
+
+        public CustomerRepository(FuminiHotelManagementContext context)
+        {
+            _context = context;
+        }
+
+        public void AddCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+        }
+
+        public void DeleteCustomer(int customerId)
+        {
+            var customer = _context.Customers.FirstOrDefault(c=>c.CustomerId == customerId);
+            _context.Customers.Remove(customer);
+            _context.SaveChanges();
+        }
+
+        public ObservableCollection<Customer> GetAllCustomers()
+        {
+            return new ObservableCollection<Customer>(_context.Customers.ToList());
+        }
+
+        public Customer GetCustomerById(int customerId)
+        {
+            var customer =_context.Customers.FirstOrDefault(c => c.CustomerId == customerId);
+            return customer;
+        }
+
+        public Customer GetCustomerByUsernameAndPassword(string username, string password)
+        {
+            return _context.Customers.FirstOrDefault(c => c.EmailAddress == username && c.Password == password);
+        }
+
+        public void UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();
+        }
+
+        ObservableCollection<BookingReservation> ICustomerRepository.GetAllCustomers()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
